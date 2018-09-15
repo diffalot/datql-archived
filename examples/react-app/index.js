c3be81a72f4f3d272385e90eda6e5c66abbd4cd2
Alice Davis <alice@diff.mx>
2018-09-15 12:03:31 -0700

    wip

--- examples/react-app/index.js
+++ examples/react-app/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const HelloMessage = props => {
   return <div>Hello {props.name}</div>
 }
 
-const mountNode = document.getElementById('app')
+const mountNode = document.getElementById('app');
 export default ReactDOM.render(<HelloMessage name="Alice" />, mountNode)
